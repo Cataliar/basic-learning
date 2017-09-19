d56de00fe9286c8f237ad0799ce852d4bd113020
Cataliar <dingji1990@qq.com>
2017-09-19 12:44:18 +0800

    correct eigthqueen

--- eightqueen/trial.c
+++ eightqueen/trial.c
@@ -4,26 +4,24 @@
 #define INF 65536
 
 int line[N];  //assume a position in each row
+int col[N];
 //y = k * x + b, in this case k = 1, 
 //so y = x + b, then one b represents a line
-int b1[N];
+int b1[2 * N];
 //y = -x + b
-int b2[N];
+int b2[2 * N];
 
 int unattack(int x, int y) {
-	int i;
-	for(i = 0; i < x; ++i) {
-		if(line[i] == y) 
-			return 0;
-	}
-	for(i = 0; i < x; ++i) {
-		if(b1[i] == y - x) 
-			return 0;
-		if(b2[i] == y + x) 
-			return 0;
-	}
+	if(col[y] == 1) 
+		return 0;
+	if(b1[y-x+N] == 1) 
+		return 0;
+	if(b2[x+y] == 1) 
+		return 0;
 	return 1;
 }
+
+int k = 0;
 
 void trial(int i, int n) {
 	//assume that we had put i-1 pieces which does not attack each other
@@ -31,14 +29,23 @@ void trial(int i, int n) {
 	//from now on we find a suitable position for line i
 	int j;
 	if(i >= n) {
+		printf("%d:", ++k);
+		for(j = 0; j < n; ++j) {
+			printf("(%d, %d)", j, line[j]);
+		}
+		printf("\n");
 		return;
 	}
 	for(j = 0; j < n; ++j) {
 		if(unattack(i, j)) {
 			line[i] = j;
-			b1[i] = j - i;
-			b2[i] = j + i;
+			col[j] = 1;
+			b1[j-i+N] = 1;
+			b2[j+i] = 1;
 			trial(i+1, n);
+			col[j] = 0;
+			b1[j-i+N] = 0;
+			b2[j+i] = 0;
 		}
 	}
 }
@@ -47,22 +54,6 @@ int main(void) {
 	int i;
 	int n = 8;
 
-	for(i = 0; i < n; ++i) {
-		b1[i] = INF;
-		b2[i] = INF;
-	}
 	trial(0, n);
-	for(i = 0; i < n; ++i) {
-		printf("%d ", b1[i]);
-	}
-	printf("\n");
-	for(i = 0; i < n; ++i) {
-		printf("%d ", b2[i]);
-	}
-	printf("\n");
-	for(i = 0; i < n; ++i) {
-		printf("(%d, %d)", i, line[i]);
-	}
-	printf("\n");
 	return 0;
 }
